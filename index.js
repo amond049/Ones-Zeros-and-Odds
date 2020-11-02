ba4a177315cd627da7cf5e4245ca870d5d3a197c
Meekyle Mondal <ammeekyle.mondal@student.tdsb.on.ca>
2020-11-02 09:37:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// Declaring some global variables 
+// Boolean variable that will contain whether or not the coin landed on heads or tails 
 var headsOrTails;
+// The starting value for the positions, -1 to ensure that the user does not have to roll one more than necessary
 var value = -1;
+// The base 10 value of the number
 var basetensum = 0;
 
-// Simulating the coin flip
+// This function flips the coin with an animation to make it look more genuine 
 function flipCoin(){
     headsOrTails = Math.floor(Math.random() * 2) + 1;
     document.getElementById("coin-display").innerHTML = "Flipping";
@@ -29,7 +31,8 @@ function flip3(){
     document.getElementById("coin-display").innerHTML = "Flipping...";
 }
 
-// Displaying whether or not it landed on heads or tails, then calling up the appropriate methods based upon what it landed on
+// This function takes the side that was flipped and then calls up the changeValue() function to, as its name suggests, change the value at that specific position to heads or 
+// tails 
 function display(){
     // Using a ternary function to check which side the "coin landed on"
     let sideDisplay = headsOrTails == 1 ? "Heads!" : "Tails!";
@@ -39,21 +42,20 @@ function display(){
     // This is where the number is assigned, make sure to check over this one
     changeValue(sideDisplay);
     if (value > 6){
-        // Deal with when it is greater than 7
         document.getElementById("flipButton").disabled = true;
-        checkWin();
     }
 }
 
-// Changing the value in both the binary display and the base 10 display
+// This function changes the value for that specific binary position and changes the base 10 value if necessary 
 function changeValue(side){
     var heads = side == "Heads!";
     if (heads){
         document.getElementById("2^" + (value + 1)).innerHTML = 1;
         basetensum += Math.pow(2, (value + 1));
+        // Mod 1000, 100, 10 to get each value
         // Getting the 100s value
         let hundredsValue = Math.floor(basetensum / 100);
-        let tensValue = Math.floor((basetensum - hundredsValue * 100) / 10);
+        let tensValue = Math.floor((basetensum - hundredsValue * 100)/ 10);
         let onesValue = basetensum % 10;
 
         document.getElementById("hundreds").innerHTML = hundredsValue;
@@ -61,17 +63,22 @@ function changeValue(side){
         document.getElementById("ones").innerHTML = onesValue;
     } else {
         document.getElementById("2^" + (value + 1)).innerHTML = 0;
+        
     }
-
+    // Changing the colour of the cells so players know what position they are at 
+    document.getElementById("label" + (value + 1)).style.backgroundColor = "green";
+    try {
+        document.getElementById("label" + (value + 2)).style.backgroundColor = "red";
+    } catch (err){
+        // Checking for the win condition 
+        if (basetensum < 140){
+            document.getElementById("coin-display").innerHTML = "You Lose!";
+        } else if (basetensum >= 140 && basetensum < 175){
+            document.getElementById("coin-display").innerHTML = "You win $1.00!";
+        } else {
+            document.getElementById("coin-display").innerHTML = "You win $2.00!";
+        }
+    }
+    // Adding one to the value so the user can move position to position from right to left 
     value += 1;
-}
-
-function checkWin(){
-    if (basetensum < 140){
-        alert("You lose!");
-    } else if (basetensum > 174){
-        alert("You win $2.00!");
-    } else {
-        alert("You get $1.00!");
-    }
 }
